d9389e20f596c3880913bbc840cbd156b789d446
Wallace <zaymanoff53@gmail.com>
2026-04-01 02:13:02 +0200

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,10 @@
 module.exports = {
   root: true,
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false,
-  },
   env: {
     node: true,
   },
   extends: [
     'plugin:vue/vue3-essential',
-    'eslint:recommended'
   ],
   rules: {}
 }
